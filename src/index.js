314b500a024e8f3f89f99c626d5904f4da11da69
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-03-03 16:29:12 +0000

    Added basic js - search form input changes h1 to input value

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+function actionSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", actionSearchSubmit);
